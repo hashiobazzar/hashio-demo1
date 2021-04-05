cb825ea32e9e50b407cc09fd89dc4e96cee2d396
hashiobazzar <yotsume.tora@gmail.com>
2021-04-05 11:37:17 +0900

    change

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
 $(function(){
-  var $window = $(window);
-  var $header = $("header");
-  var scroll_y = $window.scrollTop();
+  // function scrollWindow() {
+    // var $window = $(window);
+    // var $header = $("header");
+    // var scroll_y = $window.scrollTop();
+  //   if (scroll_y <= 0) {
+  //   $header.removeClass("is-white");
+  //   } else if (scroll_y > 0) {
+  //     $header.addClass("is-white");
+  //   }
+  // }
+
+  // $(window)
+  // .on('scroll', scrollWindow)
 
   // ヘッダーの背景
+  var $window = $(window);
+  var $header = $("header");
+
   $window.scroll(function(){
+    var scroll_y = $window.scrollTop();
     if (scroll_y <= 0) {
-      $header.removeClass("is-white");
+      console.log(scroll_y);
+      $header.removeClass("mode-white", "mode-black");
     } else {
-      $header.addClass("is-white");
+      $header.addClass("mode-white", "mode-black");
     }
-  // $header.css("background-color","red");  
+
+    if (scroll_y <= 0) {
+      console.log(scroll_y);
+      $header.removeClass("x-black");
+    } else {
+      $header.addClass("x-black");
+    }
+   
   });
+
+  // ハンバーガーメニュー
+  $(".hamburger").click(function(){
+    $(".header").toggleClass("x-active");
+    $("body").toggleClass("x-noscroll");
+    }); 
+
+  $(".header-nav-link").click(function(){
+    $(".header").removeClass("x-active");
+    $("body").removeClass("x-noscroll");
+  });  
 
   // お知らせのタブ
     $(".info-tab").on("click",function(){
   
-     $(".info-tab").removeClass("is-open");
-     $(".info-tab-content").removeClass("is-open");
-     $(this).addClass("is-open");
+     $(".info-tab").removeClass("x-open");
+     $(".info-tab-content").removeClass("x-open");
+     $(this).addClass("x-open");
      var $id = $(this).data("id");
-     $("#" + $id).addClass("is-open");
+     $("#" + $id).addClass("x-open");
   
     });
   
